8f60602783adb1265c7eb5544a77094dc69510ea
rish1299 <91524563+rish1299@users.noreply.github.com>
2024-02-15 06:37:01 +0000

    recursiononArray

---
+++ recursion/sumofArrayElements.java
@@ -0,0 +1,19 @@
+
+public class sumofArrayElements {
+
+    static int sum(int[] arr, int idx) {
+        if (idx == arr.length) {
+            return 0;
+        }
+        int smallAns = sum(arr, idx + 1);
+        return    arr[idx] + smallAns ;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 5, 20, 1 };
+        System.out.println(sum(arr, 0));
+        // System.out.println("hi");
+
+    }
+}
