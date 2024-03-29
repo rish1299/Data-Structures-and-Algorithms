26fd2713aa94de9d86f6fc74c1ced6615e26bb3e
rish1299 <91524563+rish1299@users.noreply.github.com>
2024-03-29 07:07:03 +0000

    keypadproblem

--- recursion/KeyPadCombinations.java
+++ recursion/KeyPadCombinations.java
@@ -1,15 +1,27 @@
 public class KeyPadCombinations {
-    
-    static void combination(String dig, String res){
+
+    static void combination(String dig, String[] kp, String res) { // "253" -> kp[2]
+        if (dig.length() == 0) {
+            System.out.print(res + " ");
+            return;
+        }
+
+        int currNum = dig.charAt(0) - '0'; // char '2' -> 2
+        String currChoices = kp[currNum]; // "abc"
+
+        for (int i = 0; i < currChoices.length(); i++) {
+            combination(dig.substring(1), kp, res + currChoices.charAt(i));
+        }
 
     }
-
 
     public static void main(String[] args) {
-        String s = "23";
-        String[] kp = {"","","abc","def"};
+        String dig = "23";
 
-        
+        // kp[8] = "pqrs"
+        String[] kp = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+        // 0 1 2 3 4 5 6 7 8 9
+
+        combination(dig, kp, "");
     }
 }
-
